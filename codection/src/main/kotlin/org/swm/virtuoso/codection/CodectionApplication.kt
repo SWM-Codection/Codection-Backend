9c26df2a01ee5dc44d8a24ca1e038deead3295b7
Lieslot <rlaxodud1999@gmail.com>
2024-06-02 16:34:27 +0900

    first commit

---
+++ codection/src/main/kotlin/org/swm/virtuoso/codection/CodectionApplication.kt
@@ -0,0 +1,11 @@
+package org.swm.virtuoso.codection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodectionApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodectionApplication>(*args)
+}
---
+++ codection/src/test/kotlin/org/swm/virtuoso/codection/CodectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.swm.virtuoso.codection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
